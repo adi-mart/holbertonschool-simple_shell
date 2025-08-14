8d894c4f1d88df926a21a9fcf83c31024fcce345
Krapaud <mur.mickael@gmail.com>
2025-08-14 21:50:07 +0200

    Add read_line function to handle user input in simple shell

--- test_split_func.c
+++ test_split_func.c
@@ -5,6 +5,21 @@ int is_exit_command(char *line)
 	return (strcmp(line, "exit") == 0);
 }
 
+char *read_line(void)
+{
+	char *line = NULL;
+	size_t size = 0;
+
+	if (getline(&line, &size, stdin) == -1)
+	{
+		free(line);
+		exit(0);
+	}
+	if (line[strlen(line) - 1] == '\n')
+		line[strlen(line) - 1] = '\0';
+	return (line);
+}
+
 int main(int argc, char **argv)
 {
 	char *line;
