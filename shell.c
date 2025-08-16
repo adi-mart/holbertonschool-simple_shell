d32a8997eea5d15639f3a87b5702e5590f586463
Krapaud <mur.mickael@gmail.com>
2025-08-16 13:19:41 +0200

    Remove is_exit_command function as its functionality is no longer needed

--- shell.c
+++ shell.c
@@ -1,17 +1,6 @@
 #include "shell.h"
 
 /**
- * is_exit_command - Checks if the input command is "exit"
- * @line: The input command line
- *
- * Return: 1 if command is "exit", 0 otherwise
- */
-int is_exit_command(char *line)
-{
-	return (strcmp(line, "exit") == 0);
-}
-
-/**
  * read_line - Reads a line from standard input
  *
  * Return: Pointer to the read line (must be freed by caller)
