8be1ef154a25572b8a3db8ec71219ef85320c2d6
Krapaud <mur.mickael@gmail.com>
2025-08-17 11:50:18 +0200

    Add header file for shell with function prototypes and environment variable declaration

---
+++ shell.h
@@ -0,0 +1,20 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+#define PROMPT "($) "
+
+extern char **environ;
+
+/* Function prototypes */
+char *read_line(void);
+void execute_command(char *line, char *prog_name, int count);
+int is_exit_command(char *line);
+
+#endif
