11ee3ae4b88311ae578baafabb4a721886b46a92
Your Name <you@example.com>
2025-08-19 10:39:03 +0200

    Squashed commit of the following:
    commit 89313101ee4278d8f3dc29bcc84600d26ad8be6d
    Author: Your Name <you@example.com>
    Date:   Tue Aug 19 10:38:14 2025 +0200
    
        fix: improve line parsing by handling whitespace and dynamic argument allocation

--- parsing.c
+++ parsing.c
@@ -1,26 +1,44 @@
 #include "shell.h"
 
-
 /**
- * parse_line - Splits a line into arguments
- * @line: The input line
- * Return: Array of argument strings (must be freed by caller)
+ * parse_line - Découpe la ligne en tokens (arguments)
+ * @line: La ligne de commande à parser
+ * Return: Un tableau de pointeurs vers les arguments (à free après usage)
  */
 char **parse_line(char *line)
 {
-	char **args = malloc(sizeof(char *) * MAX_ARGS);
-	char *token;
-	int idx = 0;
+	char *token, *line_copy;
+	char **args = NULL;
+	size_t bufsize = 8, i = 0;
 
+	if (!line)
+		return (NULL);
+
+	args = malloc(bufsize * sizeof(char *));
 	if (!args)
 		return (NULL);
 
-	token = strtok(line, " \t");
-	while (token && idx < MAX_ARGS - 1)
+	line_copy = strdup(line);
+	if (!line_copy)
 	{
-		args[idx++] = strdup(token);
-		token = strtok(NULL, " \t");
+		free(args);
+		return (NULL);
 	}
-	args[idx] = NULL;
+
+	token = strtok(line_copy, " \t\n");
+	while (token)
+	{
+		args[i++] = strdup(token);
+		if (i >= bufsize)
+		{
+			bufsize *= 2;
+			args = realloc(args, bufsize * sizeof(char *));
+			if (!args)
+				break;
+		}
+		token = strtok(NULL, " \t\n");
+	}
+	args[i] = NULL;
+	free(line_copy);
 	return (args);
 }
